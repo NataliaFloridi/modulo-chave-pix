e9a20d18dc2590c3824e234cb7c1b2d019e6eaf8
Leandro Ikeda Santos <nataliafloridi@gmail.com>
2025-05-01 21:58:58 -0300

    retirada de imports não usados

--- src/main/java/com/modulo/chave/pix/infrastructure/web/exception/GlobalHandlerException.java
+++ src/main/java/com/modulo/chave/pix/infrastructure/web/exception/GlobalHandlerException.java
@@ -2,8 +2,6 @@ package com.modulo.chave.pix.infrastructure.web.exception;
 
 import java.util.stream.Collectors;
 import java.time.LocalDateTime;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
